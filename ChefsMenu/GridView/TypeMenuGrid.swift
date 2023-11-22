87dbdb64b3bbe91b46ba41989c0909b186940c89
Vincenzofdg <vincenzofdg.dev@gmail.com>
2023-11-22 03:51:44 -0300

    feat: Grid With Images

---
+++ ChefsMenu/GridView/TypeMenuGrid.swift
@@ -0,0 +1,18 @@
+//
+//  TypeMenuGrid.swift
+//  ChefsMenu
+//
+//  Created by Vincenzo Di Giacomo on 22/11/23.
+//
+
+import SwiftUI
+
+struct TypeMenuGrid: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TypeMenuGrid()
+}
