309ff651107f9ab37a82c2dd1ac87725da3320e8
Vincenzofdg <vincenzofdg.dev@gmail.com>
2023-11-22 02:36:57 -0300

    feat: Start

---
+++ ChefsMenu/NavigationBar/NavigationBar.swift
@@ -0,0 +1,35 @@
+import SwiftUI
+
+struct NavigationBar: View {
+    var body: some View {
+        HStack {
+            Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
+                Image(systemName: "bell.badge")
+                    .font(.title2)
+                    .foregroundColor(.red)
+            })
+            Spacer()
+            Button("R. Antonio Ribeirão 56") {
+                
+            }
+            .font(.system(size: 18)) // or .font(.subheadline)
+            .fontWeight(.semibold)
+            .foregroundColor(.black)
+            
+            Spacer()
+            Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
+                Image(systemName: "bell.badge")
+                    .font(.title2)
+                    .foregroundColor(.red)
+                    
+            })
+        }
+
+    }
+}
+
+#Preview {
+    NavigationBar()
+        .previewLayout(.sizeThatFits)
+        .padding()
+}
