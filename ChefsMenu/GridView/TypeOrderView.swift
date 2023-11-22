077b2701c7649ecd48ce19b6fa05a6620fc1135b
Vincenzofdg <vincenzofdg.dev@gmail.com>
2023-11-22 12:13:30 -0300

    feat: gridItens

---
+++ ChefsMenu/GridView/TypeOrderView.swift
@@ -0,0 +1,18 @@
+//
+//  TypeOrderView.swift
+//  ChefsMenu
+//
+//  Created by Vincenzo Di Giacomo on 22/11/23.
+//
+
+import SwiftUI
+
+struct TypeOrderView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TypeOrderView()
+}
