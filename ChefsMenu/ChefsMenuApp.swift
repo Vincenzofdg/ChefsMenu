ddde7d71c2aa680ecad2317c32862e66b8409d72
Vincenzofdg <vincenzofdg.dev@gmail.com>
2023-11-22 01:00:05 -0300

    Initial Commit

---
+++ ChefsMenu/ChefsMenuApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChefsMenuApp.swift
+//  ChefsMenu
+//
+//  Created by Vincenzo Di Giacomo on 22/11/23.
+//
+
+import SwiftUI
+
+@main
+struct ChefsMenuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
